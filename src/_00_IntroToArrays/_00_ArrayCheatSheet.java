c6be90232219f47e714a74694480b8411b400206
Idiot <league@WTS14-4.attlocal.net>
2018-11-24 15:56:23 -0800

    11/24/18

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -1,33 +1,56 @@
 package _00_IntroToArrays;
 
-import java.util.Random;
-
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
+		// 1. make an array of 5 Strings
+		String[] array = new String[5];
+		// 2. print the third element in the array
+		System.out.println(array[2]);
+		// 3. set the third element to a different value
+		array[2] = "a";
+		// 4. print the third element again
+		System.out.println(array[2]);
+		// 5. use a for loop to set all the elements in the array to a string of your
+		// choice
+		for (int i = 0; i < 5; i++) {
+			array[i] = "A";
+		}
+		// 6. use a for loop to print all the values in the array
+		for (int i = 0; i < 5; i++) {
+			System.out.println(array[i]);
+		}
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
 
-		//2. print the third element in the array
+		// 7. make an array of 50 integers
+		double[] array2 = new double[50];
+		// 8. use a for loop to make every value of the integer array a random number
+		for (int i = 0; i < 50; i++) {
+			array2[i] = Math.random();
+		}
+		// 9. without printing the entire array, print only the smallest number on the
+		double w = 10;
+		for (int i = 0; i < 50; i++) {
+			if (array2[i] < w) {
+				w = array2[i];
+			}
+		}
+		System.out.println(w);
+		// array
 
-		//3. set the third element to a different value
+		// 10 print the entire array to see if step 8 was correct
+		for (int i = 0; i < 50; i++) {
+			System.out.println(array2[i]);
+		}
 
-		//4. print the third element again
-		
-		//5. use a for loop to set all the elements in the array to a string of your choice
-		
-		//6. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		//7. make an array of 50 integers
+		// 11. print the largest number in the array.
+		double l = 0;
+		for (int i = 0; i < 50; i++) {
+			if (array2[i] > l) {
+				l = array2[i];
+			}
+		}
+		System.out.println(l);
+		// 12. print only the last element in the array
 
-		//8. use a for loop to make every value of the integer array a random number
-
-		//9. without printing the entire array, print only the smallest number on the array
-
-		//10 print the entire array to see if step 8 was correct
-
-		//11. print the largest number in the array.
-		
-		//12. print only the last element in the array
-		
 	}
 }
